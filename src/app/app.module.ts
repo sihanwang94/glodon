6b9562ba648fe9ca3270cc4534888993f7e14099
swang94 <wangmaple2007@gmail.com>
2018-09-19 10:13:54 -0700

    antd setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
+
+/** 配置 angular i18n */
 import { registerLocaleData } from '@angular/common';
 import zh from '@angular/common/locales/zh';
 
